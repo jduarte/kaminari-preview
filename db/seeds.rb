973c9ade02424346079c3b219345c83ebfbc6769
José Duarte <jose.fduarte@gmail.com>
2014-06-12 17:53:14 +0100

    Added People, seed, and JS Pagination with forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+names = ["Jose", "Michael", "Claire", "Rory", "Nuno", "Alexandre"]
+(0..5000).each do |i|
+  Person.create! name: "#{names.shuffle.first} #{i}"
+end
